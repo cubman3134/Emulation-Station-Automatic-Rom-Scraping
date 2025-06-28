5eb2ca4e47181250854053bf53d2bd2c92c52986
Parker Holt <cubman3134@gmail.com>
2025-06-28 16:42:47 -0500

    finishing the scraper, maybe?

--- EmulationStationScripts/RetroGameTalkScraper.py
+++ EmulationStationScripts/RetroGameTalkScraper.py
@@ -1,3 +1,4 @@
+import os.path
 import requests
 from bs4 import BeautifulSoup
 import os.path
@@ -15,6 +16,7 @@ class WebScrapeInfo:
     def __init__(self, url, checkForTextInRomName, fileName):
         self.url = url
         self.checkForTextInRomName = checkForTextInRomName
+        self.fileName = fileName
 
 itemsToScrape = [WebScrapeInfo("https://retrogametalk.com/repository/translations/", "english", "TranslationUrls.txt"),
                  WebScrapeInfo("https://retrogametalk.com/repository/romhacks/", "", "RomHackUrls.txt")]
@@ -30,17 +32,7 @@ webdriverOptions.add_argument("--disable-dev-shm-usage")
 webdriverOptions.add_argument("--no-sandbox")
 webDriverInfo = webdriver.Chrome(options=webdriverOptions)
 
-dateFormatString = "%Y-%m-%d %H:%M:%S"
-#begin debug lines, delete
-f = open('c:\\temp\\logging.txt', 'a')
-f.write(datetime.now().strftime(dateFormatString))
-f.write('\n')
-f.write('\n')
-f.close()
-#end debug lines, delete
-
 def GetBeautifulSoupInfo(isPageSource, url):
-    time.sleep(0.5)
     htmlData = ''
     if isPageSource:
         htmlData = url
@@ -52,49 +44,37 @@ def GetBeautifulSoupInfo(isPageSource, url):
     return soup
 
 for itemToScrape in itemsToScrape:
+    nextButtonExists = True
     webDriverInfo.get(itemToScrape.url)
-    soup = GetBeautifulSoupInfo(True, webDriverInfo.page_source)
-    #allLinks = soup.find_all(name="td", class_="link")
-    gamesOnPage = soup.find_all(class_="game-container")
-    nextButton = soup.find(class_="next page-numbers")
-    for gameOnPage in gamesOnPage:
-        linkItem = gameOnPage.find(name="a")
-        webDriverInfo.get(linkItem['href'])
-        showLinksButton = webDriverInfo.find_element(by=By.CLASS_NAME, value="acf-get-content-button")
-        showLinksButton.click()
-        time.sleep(1)
-        childSoup = GetBeautifulSoupInfo(True, webDriverInfo.page_source)
-        platformInfo = childSoup.find(itemprop="gamePlatform operatingSystem").text
-        infoTableItems = childSoup.find(class_="rom-info").find(name="tbody").findAll(name="tr")
-        #imageFormat = ''
-        #for infoTableItem in infoTableItems:
-        #    headerItem = infoTableItem.find(name="th")
-        #    valueItem = infoTableItem.find(name="td")
-        #    if (headerItem.text == "Image Format"):
-        #        imageFormat = valueItem.Text
-
-        downloadLinksTable = childSoup.find(class_="download-links table")
-        if downloadLinksTable is None:
-            continue
-        downloadLinks = downloadLinksTable.find_all(name="a")
-        for downloadLink in downloadLinks:
-            fileNameWithExtension = downloadLink.text
-            if fileNameWithExtension is None or not itemToScrape.checkForTextInRomName.lower() in fileNameWithExtension.lower():
+    while nextButtonExists:
+        soup = GetBeautifulSoupInfo(True, webDriverInfo.page_source)
+        gamesOnPage = soup.find_all(class_="game-container")
+        nextButton = soup.find(class_="next page-numbers")
+        for gameOnPage in gamesOnPage:
+            linkItem = gameOnPage.find(name="a")
+            webDriverInfo.get(linkItem['href'])
+            showLinksButton = webDriverInfo.find_element(by=By.CLASS_NAME, value="acf-get-content-button")
+            showLinksButton.click()
+            time.sleep(0.5) # give the page time to load
+            childSoup = GetBeautifulSoupInfo(True, webDriverInfo.page_source)
+            platformInfo = childSoup.find(itemprop="gamePlatform operatingSystem").text
+            infoTableItems = childSoup.find(class_="rom-info").find(name="tbody").findAll(name="tr")
+            downloadLinksTable = childSoup.find(class_="download-links table")
+            if downloadLinksTable is None:
                 continue
-            downloadUrl = downloadLink['href']
-
-        size = sizeItem.getText()
-        if size is None or size == "-":
-            continue
-        filePath = os.path.join(systemRomPath, title + systemLink.fileType)
-        if os.path.isfile(filePath):
-            continue
-        with open(filePath, "w") as romFile:
-            romFile.write("ESDE Placeholder\n")
-            romFile.write("myrient\n") # in case I add others in the future, we know how to take each apart
-            romFile.write(systemLink.url + hrefValue + "\n")
-            romFile.write(size) # not sure if this is important but too much info isn't an issue
-    
+            downloadLinks = downloadLinksTable.find_all(name="a")
+            for downloadLink in downloadLinks:
+                fileNameWithExtension = downloadLink.text
+                if fileNameWithExtension is None or not itemToScrape.checkForTextInRomName.lower() in fileNameWithExtension.lower():
+                    continue
+                downloadUrl = downloadLink['href']
+                with open(itemToScrape.fileName, "a+") as romFile:
+                    fileNameWithoutExtension, fileExtension = os.path.splitext(fileNameWithExtension)
+                    romFile.write(downloadUrl + "\t" + fileNameWithoutExtension + "\t" + platformInfo + "\n")
+        if nextButton is None:
+            nextButtonExists = False
+        else:
+            webDriverInfo.get(nextButton['href'])
     
     
     
